8edef11ffa3c1dbc0c1d46d01f1cdecd6f1eb797
Koen Denecker <koen@polars.tech>
2026-02-25 11:16:43 +0100

    perf: Streaming cloud download for `scan_csv` (#26637)

--- crates/polars-stream/src/physical_plan/lower_ir.rs
+++ crates/polars-stream/src/physical_plan/lower_ir.rs
@@ -665,8 +665,15 @@ pub fn lower_ir(
                     }) as _,
 
                     #[cfg(feature = "csv")]
-                    FileScanIR::Csv { options } => Arc::new(Arc::clone(options)) as _,
-
+                    FileScanIR::Csv { options } => {
+                        Arc::new(crate::nodes::io_sources::csv::builder::CsvReaderBuilder {
+                            options: options.clone(),
+                            prefetch_limit: RelaxedCell::new_usize(0),
+                            prefetch_semaphore: std::sync::OnceLock::new(),
+                            shared_prefetch_wait_group_slot: Default::default(),
+                            io_metrics: std::sync::OnceLock::new(),
+                        }) as _
+                    },
                     #[cfg(feature = "json")]
                     FileScanIR::NDJson { options } => Arc::new(
                         crate::nodes::io_sources::ndjson::builder::NDJsonReaderBuilder {
@@ -677,7 +684,6 @@ pub fn lower_ir(
                             io_metrics: std::sync::OnceLock::new(),
                         },
                     ) as _,
-                    // Arc::new(options.clone()) as _,
                     #[cfg(feature = "python")]
                     FileScanIR::PythonDataset {
                         dataset_object: _,
